9f3c8d313d09e0d2e876eb4ce2a194166816daf6
kevinam99 <kevinam99@gmail.com>
2020-05-19 20:17:53 +0530

    Downloading Flask and adding requirements done

--- flask.sh
+++ flask.sh
@@ -2,3 +2,10 @@
 echo "Welcome to this utility. Let's set up your Flask project"
 
 echo "Downloading and installing Flask..."
+pip install Flask
+echo "Flask has been installed."
+
+echo "Writing to requirements..."
+touch requirements.txt
+pip freeze > requirements.txt
+echo "Requirements done."
