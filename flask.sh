634a133a1135532a8efa5e19d2317f367690155f
kevinam99 <kevinam99@gmail.com>
2020-05-19 21:00:51 +0530

    Created structure.

--- flask.sh
+++ flask.sh
@@ -5,7 +5,18 @@ echo "Downloading and installing Flask..."
 pip install Flask
 echo "Flask has been installed."
 
+
+echo "Creating project directory structure..."
+mkdir my-project
+
 echo "Writing to requirements..."
 touch requirements.txt
 pip freeze > requirements.txt
 echo "Requirements done."
+
+mkdir my-project/app
+mkdir my-project/app/templates
+
+touch myproject/run.py myproject/run.py
+cd my-project/app
+touch __init__.py views.py
