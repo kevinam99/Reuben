2443cb72505389a0b2013116ec9053d2c2d667ba
kevinam99 <kevinam99@gmail.com>
2020-05-19 21:08:33 +0530

    config.py done

--- flask.sh
+++ flask.sh
@@ -8,6 +8,8 @@ echo "Flask has been installed."
 
 echo "Creating project directory structure..."
 mkdir my-project
+cd my-project
+# pwd == my-project/
 
 echo "Writing to requirements..."
 touch requirements.txt
@@ -17,6 +19,13 @@ echo "Requirements done."
 mkdir my-project/app
 mkdir my-project/app/templates
 
-touch myproject/run.py myproject/run.py
+touch myproject/run.py myproject/config.py
 cd my-project/app
 touch __init__.py views.py
+# pwd == my-project/app
+
+cd ..
+CONFIG_PY = $'# Enable Flask\'s debugging features. Should be False in production \n DEBUG = True'
+echo "$CONFIG_PY" >> config.py
+# pwd == my-project/
+
